66ef5d203b2c55ca23ae9e2df89f9674b8ef25ac
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-09-09 12:01:18 +0530

    initial commit

---
+++ src/main/java/com/taskmanagement/salesflowx/SalesflowxApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanagement.salesflowx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesflowxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesflowxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanagement/salesflowx/SalesflowxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanagement.salesflowx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesflowxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
